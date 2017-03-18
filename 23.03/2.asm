c6a74b0d52f52c9c5fbc2630b5628ab88a7de0ef
Maslintsyn Ivan <maslintsyn@gmail.com>
2017-03-18 16:50:20 +0300

    2

---
+++ 23.03/2.asm
@@ -0,0 +1,37 @@
+.486
+.model flat, stdcall
+INCLUDE stdlib.inc
+INCLUDELIB msvcrt.lib
+
+.DATA
+  LEN_AR EQU 12
+  facts dd LEN_AR dup(?)
+
+  
+  ; произведение в edx
+  
+.CODE
+main:
+  mov ecx, LEN_AR
+  mov esi, offset facts
+  mov ebx, 1
+  mov [esi], ebx
+  
+
+start:  
+  dec ecx
+  jl stop
+  mov eax, [esi]
+  
+  mul ebx
+  add esi, TYPE facts
+  
+  inc ebx
+  mov [esi], eax
+
+  
+  jmp start
+  
+stop:
+  call exit
+END main
